5adba1aee1d6a9fc617b957eb65288b1e566733a
gioimtg2003 <conggioi.pro264@gmail.com>
2024-01-21 23:32:03 +0700

    bug Error: Illegal arguments: string, undefined

--- Backend/UserService/src/utils/hashPassword.js
+++ Backend/UserService/src/utils/hashPassword.js
@@ -1,11 +1,11 @@
-var bcrypt = require('bcryptjs');
+let bcrypt = require('bcryptjs');
 module.exports = {
     hash :  (password) =>{
-        var salt = bcrypt.genSaltSync(10); // tạo muối random
-        var hash = bcrypt.hashSync(password, salt); // hash password
+        let salt = bcrypt.genSaltSync(10); // tạo muối random
+        let hash = bcrypt.hashSync(password, salt); // hash password
         return hash;
     },
-    compare : (hashPassword) =>{
-        return bcrypt.compareSync(hashPassword, hash); // check password
+    compare : (Password, hash) =>{
+        return bcrypt.compareSync(Password, hash); // check password
     }
-}
+} 
