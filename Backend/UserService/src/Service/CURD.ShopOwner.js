e0de9d9ef47533e443f865fc2ba2ebb7648c8d67
gioimtg2003 <conggioi.pro264@gmail.com>
2024-01-21 23:32:25 +0700

    bug Error: Illegal arguments: string, undefined

--- Backend/UserService/src/Service/CURD.ShopOwner.js
+++ Backend/UserService/src/Service/CURD.ShopOwner.js
@@ -1,31 +1,77 @@
-const {Schema : SchemaShopOwner} = require('../Model/shopOwner');
+const { SchemaShopUser } = require('../Model/ShopUser');
 const log = require('../Controller/logs');
+const hashPassword = require('../utils/hashPassword');
 module.exports = {
     Create: async (ShopOwner, callback) => {
         try {
-            const newShopOwner = new SchemaShopOwner(ShopOwner)
+            let newShopOwner = new SchemaShopUser(ShopOwner)
             let save = await newShopOwner.save();
             log.logCreate(new Date, 'Success', save._id, 'Created new user')
-            callback(null, save)    
+            callback(null, save)
         } catch (err) {
             log.logCreate(new Date, 'Error', '', `Failed while create new user: ${err}`)
             callback(err, null)
         }
     },
-    Read : async (id, callback) =>{
+    Read: async (id, callback) => {
         try {
-            const ShopOwner = await SchemaShopOwner.findById(id).select ({CodeVerify : 0, ExpVerify : 0, URIVerify : 0});
+            let ShopOwner = await SchemaShopUser.findById(id).select({ CodeVerify: 0, ExpVerify: 0, URIVerify: 0 });
             if (ShopOwner) {
                 log.logInfo(new Date(), "GET_PROFILE", "Success", `Get profile successfully ${ShopOwner.Name}`);
                 callback(null, ShopOwner);
-            }else {
+            } else {
                 log.logInfo(new Date(), "GET_PROFILE", "Failed", `Not Found: ${id}`);
                 callback(null, {
-                    find : false
+                    find: false
                 });
             }
         } catch (err) {
             log.logInfo(new Date(), "GET_PROFILE", "Error", `Error while get profile: ${err}`);
+            callback(err, null);
+        }
+    },
+    Update: async (data, callback) => {
+        try {
+            let user = await SchemaShopUser.findById(data.id).select({ Password: 0, Role: 0, __v: 0, CodeVerify: 0, ExpVerify: 0, URIVerify: 0 });
+            if (user) {
+                user.Email = data.Email;
+                user.Phone = data.Phone;
+                user.Address = data.Address;
+                let saved = await user.save();
+                log.logInfo(new Date(), "UPDATE", "Success", `Update User success id: ${saved._id}`);
+                callback(null, saved);
+            } else {
+                log.logInfo(new Date(), "UPDATE", "Failed", `Update User Failed`);
+                callback("Not Found", null);
+            }
+
+        } catch (err) {
+            log.logInfo(new Date(), "UPDATE", "Error", `Update User Error`);
+            callback(err, null);
+        }
+    },
+    UpdatePassword: async (data, callback) => {
+        try {
+            let user = await SchemaShopUser.findById(data.id).select({ Password: 0, Role: 0, __v: 0, CodeVerify: 0, ExpVerify: 0, URIVerify: 0 });
+            if (user) {
+                if (hashPassword.compare(data.oldPassword, user.Password)) {
+                    user.Password = hashPassword.hash(data.newPassword)
+                    let saved = await user.save();
+                    log.logInfo(new Date(), "UPDATE", "Success", `Update Password: ${saved._id}`);
+                    callback(null, saved);
+                } else {
+                    log.logInfo(new Date(), "UPDATE", "Failed", `Password does not match: ${data.oldPassword}`);
+                    callback("Password does not match", null);
+                }
+
+            } else {
+                log.logInfo(new Date(), "UPDATE", "Failed", `Update Password User Failed`);
+                callback("Not Found", null);
+            }
+
+        } catch (err) {
+            console.error(err)
+            log.logInfo(new Date(), "UPDATE", "Error", `Update Password User Error`);
             callback(err, null);
         }
     }
