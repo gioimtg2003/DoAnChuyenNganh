3920053b020ba5e7f6fa2c8828cf37003a753406
gioimtg2003 <conggioi.pro264@gmail.com>
2024-01-21 23:31:27 +0700

    update Route update

--- Backend/UserService/src/Routes/CURD.ShopOwner.js
+++ Backend/UserService/src/Routes/CURD.ShopOwner.js
@@ -1,11 +1,15 @@
 const route = require('express').Router();
-const CreateUser = require('../Controller/Create.ShopOwner').CreateShopOwner;
-const GetProfile = require('../Controller/Get.ShopOwner').getProfile;
+const Create = require('../Controller/CreateUser');
+const GetProfile = require('../Controller/GetProfile');
 const middle = require('../middleware/Check.Attribute');
+const Update = require('../Controller/UpdateUser');
 
+route.post('/shopowner/register', middle.REGISTER, Create.ShopOwner);
+route.get('/shopowner/:id', GetProfile.ShopOwner);
+route.post("/shopowner/update/profile", Update.ProfileShopOwner);
+//Bug Update Password
+// route.post("/shopowner/update/password", Update.PasswordShopOwner);
 
-route.post('/shopowner/register', middle.REGISTER, CreateUser);
-route.get('/shopowner/:id', GetProfile);
 
 module.exports = {
     route : route
