ff85d350bf7ced5ba37a3ec3cb7bc32c2395baeb
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-02 13:23:13 +0700

    #9 Feature: check verify user

--- Backend/UserService/src/Controller/GetProfile.js
+++ Backend/UserService/src/Controller/GetProfile.js
@@ -1,15 +1,15 @@
-const {Read} = require('../Service/CURD.ShopOwner');
+const { Read } = require('../Service/CURD.ShopOwner');
 const HTTP_CODE = require('../config/status.http');
-const API  = require('./res/Api.Response').res;
+const API = require('./res/Api.Response').res;
 module.exports = {
-    ShopOwner : (req, res) =>{
-        const {id} = req.params;
-        Read(id, (err, data) =>{
+    ShopOwner: (req, res) => {
+        const { id } = req.params;
+        Read(id, (err, data) => {
             if (err) {
-                let APIRes = new API (HTTP_CODE.INTERNAL_ERROR, 'Error', err, 'Error While get profile user');
+                let APIRes = new API(HTTP_CODE.INTERNAL_ERROR, 'Error', err, 'Error While get profile user');
                 return res.status(HTTP_CODE.INTERNAL_ERROR).json(APIRes.APIReturn());
-            }else if (data) {
-                let APIRes = new API (HTTP_CODE.OK, 'Succes', data, 'Get User Success');
+            } else if (data) {
+                let APIRes = new API(HTTP_CODE.OK, 'Success', data, 'Get User Success');
                 return res.status(HTTP_CODE.OK).json(APIRes.APIReturn());
             }
         })
--- Backend/UserService/src/Routes/CURD.ShopOwner.js
+++ Backend/UserService/src/Routes/CURD.ShopOwner.js
@@ -4,9 +4,10 @@ const GetProfile = require('../Controller/GetProfile');
 const middle = require('../middleware/Check.Attribute');
 const Update = require('../Controller/UpdateUser');
 const { ValidateInputRegister } = require("../middleware/Validate");
-route.post('/shopowner/register', middle.REGISTER, ValidateInputRegister, Create.ShopOwner);
-route.get('/shopowner/:id', GetProfile.ShopOwner);
-route.post("/shopowner/update/profile", Update.ProfileShopOwner);
+const { CheckVerifyUser } = require('../middleware/CheckVerifyUser');
+route.post('/shopowner/', middle.REGISTER, ValidateInputRegister, Create.ShopOwner);
+route.get('/shopowner/:id', CheckVerifyUser, GetProfile.ShopOwner);
+route.put("/shopowner/", Update.ProfileShopOwner);
 //Bug Update Password
 // route.post("/shopowner/update/password", Update.PasswordShopOwner);
 
