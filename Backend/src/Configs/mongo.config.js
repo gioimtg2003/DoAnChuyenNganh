9dacea75fdf20019127d5a399bb7260d07975883
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-02 16:34:21 +0700

    update code: config mongo server

---
+++ Backend/src/Configs/mongo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+    SHIPPER_MODEL: "shippers_users",
+    SHOP_MODEL: "shop_users",
+    PRODUCT_MODEL: "products",
+    AUTH_MODEL: "auths",
+    ORDER_MODEL: "orders"
+}
