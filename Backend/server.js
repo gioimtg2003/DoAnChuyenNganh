debfa192e11c8be9971230171ad70896a2594944
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-02 16:24:00 +0700

    update code: server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,17 @@
-// import libs
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
+const { PORT } = require("./src/Configs/sys.config");
+
 // config server
-require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api");
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
