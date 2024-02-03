e151a7eaa6badd3a56ce1cd6871c3b3f1b7b0d74
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-03 21:49:29 +0700

    #8 Feature: Validate Register Input

--- Backend/src/Controllers/User/CURDShop.js
+++ Backend/src/Controllers/User/CURDShop.js
@@ -1,4 +1,5 @@
 const hashPassword = require("../../../../Microservice/UserService/src/utils/hashPassword");
+const { InputValidate } = require("../../../../Microservice/UserService/src/utils/validateInput");
 const { OK, INTERNAL_ERROR, BAD_REQUEST } = require("../../Configs/HTTPCode");
 const { ServiceCreateShop } = require("../../Services/User/Shop/CURDShop");
 const { API } = require("../../Utils/formatApi");
@@ -6,6 +7,13 @@ function CreateShop(req, res) {
     const requiredFields = ['Email', 'Password', 'Name', 'Phone', 'Address', 'Scope', 'ShopName', 'ShopAddress', 'Role'];
     const { body } = req;
     const { Email, Password, Name, Phone, Address, Scope, ShopName, ShopAddress, Role } = body;
+    const isValidEmail = InputValidate("Email", Email);
+    const isValidPhone = InputValidate("Phone", Phone);
+    const isValidRole = InputValidate("Role", Role);
+    if (!(isValidEmail && isValidPhone && isValidRole)) {
+        let api = API(BAD_REQUEST, "failed", `Input valid fields: ${!isValidEmail ? "Email " : ""}${!isValidPhone ? "Phone " : ""}${!isValidRole ? "Role " : ""}Invalid Input`, {}, new Date());
+        return res.status(BAD_REQUEST).json(api);
+    }
     let missingFields = requiredFields.filter(field => !body[field]);
     if (missingFields.length > 0) {
         let api = API(BAD_REQUEST, "failed", "Missing the fields", {}, new Date())
