bc7c0f6c097188f91cf223da9974c3b5ae2129b5
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-03 08:57:39 +0700

    Code model Shop Owner

---
+++ Backend/src/Models/Users/ShopModel.js
@@ -0,0 +1,60 @@
+let mongoose = require('../../db/Connect.Mongo').mongoose;
+
+let ShopUser = new mongoose.Schema({
+    Email: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 100,
+        unique: true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Phone: {
+        type: String,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    Scope: {
+        type: Boolean,
+        required: true,
+    },
+    ShopName: {
+        type: String,
+        require: true
+    },
+    ShopAddress: {
+        type: String,
+        required: true
+    },
+    Verify: {
+        type: Boolean,
+        required: true
+    },
+    CodeVerify: {
+        type: Number,
+    },
+    URIVerify: {
+        type: String,
+    },
+    ExpVerify: {
+        type: Number,
+    },
+    Role: {
+        type: Number,
+        required: true
+    }
+
+});
+module.exports = {
+    SchemaShopUser: mongoose.model("shopusers", ShopUser)
+}
