a14f20e1fbb4d66b4eea319e6e6478fbf7c1125a
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-03 08:52:25 +0700

    Code Log connect database mongo

--- Backend/src/db/Connect.Mongo.js
+++ Backend/src/db/Connect.Mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const { MONGO_URI, DATABASE_NAME, MONGO_USER, MONGO_PASSWORD } = require("../Configs/mongo.config");
-const log = require('../Controller/logs');
-
+const { logInfo, logError } = require("../Utils/logger");
 let connect = async () => {
     try {
         console.log(configDB.DATABASE_NAME)
@@ -10,10 +9,9 @@ let connect = async () => {
             user: MONGO_USER,
             pass: MONGO_PASSWORD
         });
-        log.logConn(new Date(), 'Success', `[Connect MongoDB] Successfully - Database Name: ${configDB.DATABASE_NAME}`)
+        logInfo(new Date(), "Success", `Connect to Database successfully Database name: ${DATABASE_NAME}`, "Connect MongoDB")
     } catch (err) {
-        console.log(configDB.URI)
-        log.logConn(new Date(), 'Error', `[Connect MongoDB] ${err}`)
+        logError(new Date(), `Connect to Database Error: ${err}`, "Connect MongoDB")
     }
 }
 connect();
