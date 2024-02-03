51e4ce684c5d02eaa43737ed7822063799f0f79e
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-03 09:03:19 +0700

    Code Route for user service

---
+++ Backend/src/Routes/index.js
@@ -0,0 +1,14 @@
+const route = require("express").Router();
+
+
+//-----Route for user service-----//
+// Shop
+route.get("/user/shop");
+route.post("/user/shop");
+route.put("/user/shop");
+route.delete("/user/shop");
+//Shipper
+route.get("/user/shipper");
+route.post("/user/shipper");
+route.put("/user/shipper");
+route.delete("/user/shipper");
