01cedcd0316e36aa02cbd9ed6d74b8a923c99ba2
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-03 08:49:09 +0700

    Code log info & error

--- Backend/src/Utils/logger.js
+++ Backend/src/Utils/logger.js
@@ -1,25 +1,35 @@
-const winston = require('winston');
-const { format, transports } = require("winston");
-const logger = winston.createLogger({
-    format: format.combine(
-        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
-        format.align(),
-        format.printf((i) => `${i.level}: ${[i.timestamp]}: ${i.message}`)
-    ),
-    transports: [
-        new transports.File({
-            filename: "logs/info.log",
-            level: "info",
-            format: format.combine(
-                format.printf((i) =>
-                    i.level === "info" ? `${i.level}: ${i.timestamp} ${i.message}` : ""
-                )
-            ),
-        }),
-        new transports.File({
-            filename: "logs/error.log",
-            level: "error",
-        }),
-    ],
-})
-logger.error("ahihiâsssashi")
+// const winston = require('winston');
+// const { format, transports } = require("winston");
+// const logger = winston.createLogger({
+//     format: format.combine(
+//         format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+//         format.align(),
+//         format.printf((i) => `${i.level}: ${[i.timestamp]}: ${i.message}`)
+//     ),
+//     transports: [
+//         new transports.File({
+//             filename: "logs/info.log",
+//             level: "info",
+//             format: format.combine(
+//                 format.printf((i) =>
+//                     i.level === "info" ? `${i.level}: ${i.timestamp} ${i.message}` : ""
+//                 )
+//             ),
+//         }),
+//         new transports.File({
+//             filename: "logs/error.log",
+//             level: "error",
+//         }),
+//     ],
+// })
+
+const formatDate = require("./formatDate");
+
+module.exports = {
+    logInfo: (timeNow, status, msg, service) => {
+        console.log(`[${formatDate(timeNow)}] Status: ${status} - ${msg} - Service: ${service}`);
+    },
+    logError: (timeNow, msg, service) => {
+        console.error(`[${formatDate(timeNow)}] Status: Error - ${msg} - Service: ${service}`);
+    },
+}
