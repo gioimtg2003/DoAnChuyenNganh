204dcf520fd02c6ad40c6fcda19215df33fa67e2
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-06 21:50:34 +0700

    Update code

---
+++ Backend/src/Utils/validateInput.js
@@ -0,0 +1,17 @@
+
+const regexPatterns = {
+    Email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    Phone: /^(0|\+84)[1-9]\d{8}$/,
+    UserName: /^[a-zA-Z0-9]+$/,
+    Password: /.*/,
+    Role: /[1-3]/
+};
+
+function InputValidate(type, value) {
+    const regex = regexPatterns[type];
+    return regex.test(value);
+}
+
+module.exports = {
+    InputValidate
+};
