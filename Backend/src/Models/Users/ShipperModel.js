a2849fff48fb557510339b27b312657eb319966c
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-07 10:24:56 +0700

    Code :Shipper Model

---
+++ Backend/src/Models/Users/ShipperModel.js
@@ -0,0 +1,55 @@
+let mongoose = require('../../db/Connect.Mongo').mongoose;
+
+let Shipper = new mongoose.Schema({
+    Email: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 100,
+        unique: true
+    },
+    Password: {
+        type: String,
+        min: 6,
+        max: 100,
+        required: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Phone: {
+        type: String,
+        max: 10,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    Verify: {
+        type: Boolean,
+        required: true
+    },
+    CodeVerify: {
+        type: Number,
+    },
+    URIVerify: {
+        type: String,
+    },
+    ExpVerify: {
+        type: Number,
+    },
+    Role: {
+        type: Number,
+        required: true
+    },
+    Delete: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+module.exports = {
+    SchemaShipper: mongoose.model("shippers", Shipper)
+}
