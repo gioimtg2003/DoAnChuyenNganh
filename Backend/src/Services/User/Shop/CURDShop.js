021fdd65ce48c78570d258afe09ba083d46812e6
gioimtg2003 <conggioi.pro264@gmail.com>
2024-02-07 10:07:33 +0700

    #30 Feature Update user

--- Backend/src/Services/User/Shop/CURDShop.js
+++ Backend/src/Services/User/Shop/CURDShop.js
@@ -19,8 +19,35 @@ async function ServiceCreateShop(data, callback) {
         callback(err, null)
     }
 }
+/**
+ * This function Update resources based on `schema shop user`
+ * @param {*} data include : `id`, `information`
+ * @param {*} callback `if err` return internal error server `else` return data
+ * @example console.log('Hello world')
+ */
+async function ServiceUpdateShop(data, callback) {
+    try {
+        const { id } = data;
+        const info = data.data;
+        let user = await SchemaShopUser.findOneAndUpdate({ _id: id }, info, { new: true });
+        if (user) {
+            logInfo(new Date, "Success", `Update a user: ${user._id}`, "Update User");
+            callback(null, data);
+        } else {
+            logInfo(new Date, "Failed", `Update a user`, "Update User");
+            callback(null, false);
+        }
 
+    } catch (err) {
+        logError(new Date, `Update user error: ${err}`, "Update User");
+        callback(err, null);
+
+    }
+
+
+}
 
 module.exports = {
-    ServiceCreateShop: ServiceCreateShop
+    ServiceCreateShop: ServiceCreateShop,
+    ServiceUpdateShop: ServiceUpdateShop
 }
