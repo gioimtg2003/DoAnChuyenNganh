6660433e3e61a3d6613f27b046a318c6927e51cc
gioimtg2003 <conggioi.pro264@gmail.com>
2024-03-12 00:16:01 +0700

    feat: check login, useToken

---
+++ Mobile/src/lib/context/Auth/AuthContext.tsx
@@ -0,0 +1,80 @@
+import React, {
+  Reducer,
+  createContext,
+  useCallback,
+  useContext,
+  useReducer,
+} from "react";
+import { AuthAction, AuthActionType, AuthState } from "./type";
+import { reducerAuthHandler } from "./reducer";
+import { useToken, IUseToken } from "../../hooks/useToken";
+
+const initialState: AuthState = {
+  isAuthenticated: false,
+  isLoading: true,
+};
+
+const AuthSource = () => {
+  const [state, dispatch] = useReducer<Reducer<AuthState, AuthAction>>(
+    reducerAuthHandler,
+    initialState
+  );
+  const { setAccessToken, setRefreshToken, setExp, removeToken } = useToken();
+
+  const handleInitialize = useCallback(() => {
+    dispatch({
+      type: AuthActionType.INITIALIZE,
+      payload: { isAuthenticated: false, isLoading: false },
+    });
+  }, []);
+
+  const handleLogin = useCallback(async (token: IUseToken) => {
+    try {
+      await setAccessToken(token.accessToken);
+      await setRefreshToken(token.refreshToken);
+      await setExp(token.exp);
+      dispatch({
+        type: AuthActionType.LOGIN,
+        payload: { isAuthenticated: true, isLoading: true },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleLogout = useCallback(async () => {
+    try {
+      await removeToken();
+      dispatch({
+        type: AuthActionType.LOGOUT,
+        payload: { isAuthenticated: false, isLoading: false },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return { state, handleInitialize, handleLogin, handleLogout };
+};
+
+const AuthContext = createContext<ReturnType<typeof AuthSource>>({} as any);
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+}
+
+const AuthProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
+  return (
+    <AuthContext.Provider value={AuthSource()}>{children}</AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, useAuth };
---
+++ Mobile/src/lib/hooks/useToken.ts
@@ -0,0 +1,64 @@
+import AsyncStorage, {
+  useAsyncStorage,
+} from "@react-native-async-storage/async-storage";
+import { useCallback } from "react";
+import { STORAGE_KEY } from "../Constant";
+
+export interface IUseToken {
+  accessToken: string;
+  refreshToken: string;
+  exp: number;
+}
+type UseToken = {
+  getAccessToken: () => Promise<string | null>;
+  getRefreshToken: () => Promise<string | null>;
+  setAccessToken: (token: string) => Promise<void>;
+  setRefreshToken: (token: string) => Promise<void>;
+  removeToken: () => Promise<void>;
+  setExp: (exp: number) => Promise<void>;
+  getExp: () => Promise<string | null>;
+};
+
+export const useToken = (): UseToken => {
+  const getAccessToken = useCallback(async () => {
+    let token = await AsyncStorage.getItem(STORAGE_KEY.ACCESS_TOKEN);
+    return token;
+  }, []);
+
+  const getRefreshToken = useCallback(async () => {
+    let token = await AsyncStorage.getItem(STORAGE_KEY.REFRESH_TOKEN);
+    return token;
+  }, []);
+
+  const setAccessToken = useCallback(async (token: string) => {
+    await AsyncStorage.setItem(STORAGE_KEY.ACCESS_TOKEN, token);
+  }, []);
+
+  const setRefreshToken = useCallback(async (token: string) => {
+    await AsyncStorage.setItem(STORAGE_KEY.REFRESH_TOKEN, token);
+  }, []);
+
+  const removeToken = useCallback(async () => {
+    await AsyncStorage.removeItem(STORAGE_KEY.ACCESS_TOKEN);
+    await AsyncStorage.removeItem(STORAGE_KEY.REFRESH_TOKEN);
+    await AsyncStorage.removeItem(STORAGE_KEY.EXP);
+  }, []);
+
+  const setExp = useCallback(async (exp: number) => {
+    await AsyncStorage.setItem(STORAGE_KEY.EXP, exp.toString());
+  }, []);
+
+  const getExp = useCallback(async () => {
+    let exp = await AsyncStorage.getItem(STORAGE_KEY.EXP);
+    return exp;
+  }, []);
+  return {
+    getAccessToken,
+    getRefreshToken,
+    setAccessToken,
+    setRefreshToken,
+    removeToken,
+    setExp,
+    getExp,
+  };
+};
